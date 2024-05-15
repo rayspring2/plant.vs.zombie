62e7abf0cfa42839b0d7dbb289718078eb1a4c35
Mahdis <mhdys5998@gmail.com>
2024-05-15 08:09:00 +0330

    fix bug

--- include/Item.hpp
+++ include/Item.hpp
@@ -3,6 +3,17 @@
 #define ITEM_INCLUDE
 #include "Global.hpp"
 
+const int AVAILABLE_TOP = 0;
+const int UNAVAILABLE_TOP = 60;
+const int WIDTH_OF_ITEM = 100;
+const sf::Color CLICK_MODE_COLOR = sf::Color(255, 255, 255, 128);
+const int HEIGHT_OF_ITEM = 60;
+const int FONT_SIZE = 24;
+const int TEXT_POS_X = 40;
+const int TEXT_POS_Y = 20;
+const int PRICE_TEXT_X = 65;
+const int PRICE_TEXT_Y = 35;
+
 class Item {
 public:
 	Item(int x, int y, PlantType plant_type, int cooldown, string path, int price);
@@ -19,7 +30,6 @@ public:
     Clock clock;
     Texture texture;
     Sprite sprite;
-    void increase_current_time();
     void setShowBorder(bool show_value) {show_border = show_value;}
     bool getShowBorder() {return show_border;}
 
@@ -28,6 +38,7 @@ public:
 protected:
 
 private:
+
     bool show_border;
     int price;
     int cooldown;
@@ -36,6 +47,7 @@ private:
     IntRect rect;
     Font font;
     Text text;
+    Text price_text;
     PlantType plant_type;
 };
 
--- src/Item.cpp
+++ src/Item.cpp
@@ -7,14 +7,14 @@ void Item :: fixTexture(string path) {
     }
     sprite.setTexture(texture);
     IntRect rect;
-    rect.width = 100;
-    rect.height = 60;
+    rect.width = WIDTH_OF_ITEM;
+    rect.height = HEIGHT_OF_ITEM;
     sprite.setTextureRect(rect);
 }
 
 void Item :: makeBorder(RenderWindow &window) {
     show_border = true;
-    sprite.setColor(sf::Color(255, 255, 255, 128));
+    sprite.setColor(CLICK_MODE_COLOR);
 }
 
 Item :: Item(int x, int y, PlantType plant_type, int cool_down, string path, int price) : cooldown(cool_down), plant_type(plant_type), price(price) {
@@ -27,17 +27,25 @@ Item :: Item(int x, int y, PlantType plant_type, int cool_down, string path, int
     fixTexture(path);
     text.setFont(font);
     text.setColor(Color :: Red);
-    text.setCharacterSize(24);
-    Vector2f text_pos = {sprite.getPosition().x + 50, sprite.getPosition().y + 30};
+    text.setCharacterSize(FONT_SIZE);
+    Vector2f text_pos = {sprite.getPosition().x + TEXT_POS_X, sprite.getPosition().y + TEXT_POS_Y};
     text.setPosition(text_pos);
+
+    price_text.setFont(font);
+    price_text.setColor(Color :: Black);
+    price_text.setCharacterSize(FONT_SIZE);
+    Vector2f price_text_pos = {sprite.getPosition().x + PRICE_TEXT_X, sprite.getPosition().y + PRICE_TEXT_Y};
+    price_text.setPosition(price_text_pos);
+    price_text.setString(to_string(price));
 }
+
 
 void Item :: update(int money) {
     if(item_state == LOADING) {
         IntRect rect;
-        rect.top = 60;
-        rect.width = 100;
-        rect.height = 60;
+        rect.top = UNAVAILABLE_TOP;
+        rect.width = WIDTH_OF_ITEM;
+        rect.height = HEIGHT_OF_ITEM;
         sprite.setTextureRect(rect);
         int elapsed = clock.getElapsedTime().asSeconds();
         if(elapsed>= cooldown) {
@@ -50,9 +58,9 @@ void Item :: update(int money) {
     }
     if(item_state == UNAVAILABLE) {
         IntRect rect;
-        rect.top = 60;
-        rect.width = 100;
-        rect.height = 60;
+        rect.top = UNAVAILABLE_TOP;
+        rect.width = WIDTH_OF_ITEM;
+        rect.height = HEIGHT_OF_ITEM;
         sprite.setTextureRect(rect);
         if(money >= price) {
             item_state = AVAILABLE;
@@ -60,9 +68,9 @@ void Item :: update(int money) {
     }
     if(item_state == AVAILABLE) {
         IntRect rect;
-        rect.top = 0;
-        rect.width = 100;
-        rect.height = 60;
+        rect.top = AVAILABLE_TOP;
+        rect.width = WIDTH_OF_ITEM;
+        rect.height = HEIGHT_OF_ITEM;
         sprite.setTextureRect(rect);
     }
     if (price > money && item_state == AVAILABLE) {
@@ -72,6 +80,7 @@ void Item :: update(int money) {
 
 void Item :: render(RenderWindow &window) {
     window.draw(sprite);
+    window.draw(price_text);
     if(item_state == LOADING) {
         window.draw(text);
     }
@@ -81,9 +90,3 @@ void Item :: render(RenderWindow &window) {
 ItemState Item :: getStatus() {
     return item_state;
 }
-
-//
-void Item::increase_current_time() {
-    current_time += 100;
-}
-//
